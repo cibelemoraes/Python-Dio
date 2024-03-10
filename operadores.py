723b2de2b7646442a9ee0e35d3921668c5b827b0
Your Name <you@example.com>
2024-03-10 18:27:53 -0300

    Operadores Python

---
+++ operadores.py
@@ -0,0 +1,39 @@
+#print(10 - 5 * 2)
+#print ((10 -5) * 2)
+#print (10**2 * 2)
+#print(10/2 * 4)
+
+"""produto_1 = 30
+produto_2 = 20
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)"""
+
+"""x = (10 +5) * 4
+y = (10 / 2) + 25 * ((2 +2 )**2)
+print (x)
+print (y)"""
+
+"""saldo = 200
+saque = 200
+print( saldo == saque)
+print( saldo != saque)
+print( saldo > saque)
+print( saldo < saque)
+print( saldo >= saque)
+print( saldo <= saque)"""
+
+"""saldo = 500
+print( saldo )
+saldo = 200
+print (saldo)"""
+
+"""saldo = 1000
+print( saldo)
+saldo +=50
+print (saldo)"""
+
+
---
+++ operadoresLogicos.py
@@ -0,0 +1,5 @@
+saldo = 1000
+saque = 200
+limite = 100
+
+saldo >= saque and saque <= limite
