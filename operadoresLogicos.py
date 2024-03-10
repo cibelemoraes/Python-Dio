ab12062626fdae980842f9bfa21950f9dea4026f
Your Name <you@example.com>
2024-03-10 18:45:33 -0300

    operadores and, or

--- operadoresLogicos.py
+++ operadoresLogicos.py
@@ -1,5 +1,23 @@
-saldo = 1000
-saque = 200
-limite = 100
+# AND = para ser True tudo tem que True
+# Or = para ser True  um tem que ser True
 
-saldo >= saque and saque <= limite
+""""print(True and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or False)"""
+
+
+"""saldo = 1000
+saque = 250
+limite = 200
+conta_especial = True
+
+exp = saldo >= saque and saque <= limite or conta_especial and saldo >= saque
+print(exp)"""
+
+saldo = 1000
+limite = 1000
+print(saldo is limite)
+print(saldo is not limite)
