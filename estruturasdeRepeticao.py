4af558e115f0280094ebb2f50991828749390486
Your Name <you@example.com>
2024-03-12 11:43:02 -0300

    estruturas de repetição

---
+++ estruturasdeRepeticao.py
@@ -0,0 +1,36 @@
+"""texto = input("informe um texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end="")
+
+else:
+    print()
+    print("Execute no final do laço")"""
+
+"""opcao = -1
+
+while opcao != 0:
+    opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n: "))
+
+    if opcao == 1:
+        print ("Sacando...")
+    elif opcao == 2:
+        print("Exibindo o Extrato...")
+else:
+   print("Obrigado por utilizar nosso progama")"""
+
+"""while True:
+    numero = int(input("imforme um numero: "))
+
+    if numero == 10:
+        break
+
+    print(numero)"""
+
+for numero in range(100):
+
+    if numero == 12:
+        break
+    print(numero, end=" ")
