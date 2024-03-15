986783bf2603830efef267bbc328e4eb474c45a4
Your Name <you@example.com>
2024-03-14 22:49:16 -0300

    pythonStrings

---
+++ pythonString.py
@@ -0,0 +1,37 @@
+=nome = "GuILhERlme"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "   Ola Mundo   "
+
+print(texto)
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.ltrip() + ".")
+
+"""menu = "   Olá Mundo   "
+
+print("####" +menu+ "#####")
+print(menu.center(14))
+print(menu.center(20, "#"))
+print("-".join(menu))"""
+
+"""nome = "Guilherme"
+idade = "28"
+profissao = "progamador"
+linguagem = "Python"
+
+dados = {"nome":"guilherme","idade" :28}
+
+
+print("Nome: {} Idade: {}".format(nome, idade))
+print("Nome: {1} Idade: {0} Nome: {1} {1}".format(idade,nome))
+
+print("Nome: {nome} Idade:{idade}".format(nome=nome, idade=idade))
+print("Nome: {name} Idade:{age} {name}{age} {name}".format(age=idade, name=nome))
+print("Nome: {nome} Idade:{idade}".format(**dados))
+
+print(f"Nome: {nome} Idade:{idade}")"""
+
