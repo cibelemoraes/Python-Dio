7f1f109b46dbc1289342014d1aa2bf4446bff6ad
Your Name <you@example.com>
2024-03-21 18:37:27 -0300

    tuplas modificação

---
+++ Estruturas com Python/tuplas.py
@@ -0,0 +1,12 @@
+matriz = (
+    (1, "a", 2),
+    ("b", 3, 4),
+    (6, 5, "c"),
+)
+
+print(matriz[0])
+print(matriz[0][0])
+print(matriz[0][-1])
+print(matriz[-1][-1])
+
+#a tupla não permite alterar o valor dos elementos 
