8b2e9182ef6b338c5aae67d5561e9e830469cee2
Your Name <you@example.com>
2024-03-21 18:21:38 -0300

    estruturas com python

---
+++ Estruturas com Python/Listaspython.py
@@ -0,0 +1,24 @@
+"""frutas = ["laranja", "maça", "uva"]
+frutas = [1] #maça
+carro = ["Ferrari", "F8", "4200"]"""
+
+#copiando uma lista
+
+"""lista = [1, "PYTHON", [40, 30, 20]]
+#copiando a lista
+
+l2 = lista.copy()
+#print(lista)
+print(id(l2), id(lista))
+
+l2[0]= 2
+print(l2)
+print(lista)"""
+
+#para saber a primeira ocorrencia de umitem nuam lista
+"""linguagens = ["python", "java", "c", "java", "csharp"]
+
+print(linguagens.index("java"))
+print(linguagens.index("python"))"""
+
+
