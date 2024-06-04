a940964ba2effb04cc7aa0c8988c653997ff9f5c
Your Name <you@example.com>
2024-06-04 12:42:53 -0300

    hora

--- Data e Hora/custom_datetime.py
+++ Data e Hora/custom_datetime.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import date, datetime, time
 
 data = date(2024, 4, 6)
 print(data)
@@ -6,3 +6,7 @@ print(data.today())
 
 data_hora= datetime(2024, 4, 6, 12, 39, 20)
 print(data_hora)
+print(datetime.today())
+
+hora = time (10, 20, 00)
+print(hora)
