48acab95c702683c8e0f77b4a979843af24fd73d
Your Name <you@example.com>
2024-06-04 10:43:57 -0300

    decorador c argumentos

--- Decoradores- Iterativos/decorador_c_argumentis.py
+++ Decoradores- Iterativos/decorador_c_argumentis.py
@@ -1,14 +1,14 @@
 def meu_decorador(funcao):
-    def envelope():
+    def envelope(*args, **kwargs):
         print("faz algo antes de executar")
-        funcao()
+        resultado = funcao(*args, **kwargs)
         print("faz algo depois de executar")
-        
+        return resultado
     return envelope
 
 
 @meu_decorador
 def ola_mundo(nome):
-    print("Ola Mundo!")
+    print("Ola Mundo!", nome)
 
-ola_mundo()
+ola_mundo("João")
