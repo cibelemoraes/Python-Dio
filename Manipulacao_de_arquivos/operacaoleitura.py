4ba53024d710158e56b8f8be571e5cfed368a230
Your Name <you@example.com>
2024-06-05 15:09:18 -0300

    leitura de arquivos

---
+++ Manipulacao_de_arquivos/operacaoleitura.py
@@ -0,0 +1,5 @@
+arquivo = open(r'C:\Users\cibel\Downloads\video visagio.txt')  # Usando uma string raw para evitar problemas com caracteres de escape
+
+#print(arquivo.read())
+print(arquivo.readline())
+arquivo.close()
